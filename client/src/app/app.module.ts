394518885047289089253fa8cef51241204ef91d
MonseCastanon <monsecastanon023@gmail.com>
2025-03-09 10:49:09 -0600

    Cambios aceptados

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductoComponent } from './components/producto/producto.component';
+import { AlmacenistaComponent } from './components/almacenista/almacenista.component';
+import { VendedorComponent } from './components/vendedor/vendedor.component';
+import { ClienteComponent } from './components/cliente/cliente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductoComponent,
+    AlmacenistaComponent,
+    VendedorComponent,
+    ClienteComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/components/cliente/cliente.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cliente',
+  standalone: false,
+  templateUrl: './cliente.component.html',
+  styleUrl: './cliente.component.css'
+})
+export class ClienteComponent {
+
+}
