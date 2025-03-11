58cc186dc4e6b5be1edb4c5c9baa206d11eadf0a
MonseCastanon <monsecastanon023@gmail.com>
2025-03-11 16:02:05 -0600

    Actualización de agregar producto

--- client/src/app/almacenista/pages/dashboard/dashboard.component.ts
+++ client/src/app/almacenista/pages/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
+declare var $: any;
+
 @Component({
   selector: 'app-dashboard',
   standalone: false,
@@ -12,5 +14,9 @@ export class DashboardComponent {
 
   isDashboardRoot(): boolean {
     return this.router.url === '/dashboard';
+  }
+
+  openInventoryModal() {
+    $('#inventoryModal').modal('show'); // Abre el modal
   }
 }
