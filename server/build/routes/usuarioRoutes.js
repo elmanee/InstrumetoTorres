c3f147033974b37804e3d303246be906f25de79f
elmanee <cp457920@gmail.com>
2025-03-17 15:34:36 -0600

    Seguro
    commit seguros

--- server/build/routes/usuarioRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const clienteControllers_1 = require("../controllers/clienteControllers");
-const router = (0, express_1.Router)();
-router.post('/register', clienteControllers_1.registerUsuario);
-router.post('/login', clienteControllers_1.loginUsuario);
-exports.default = router;
